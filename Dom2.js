fd0aef13401436364ab226be71646105560b6001
prerna1233 <prernasai23@navgurukul.org>
2024-08-06 11:00:12 +0530

    Add files via upload

---
+++ Dom2.js
@@ -0,0 +1,93 @@
+let b = document.createElement("body")
+let div = document.createElement('div');
+document.body.appendChild(div);
+div.classList.add('container');
+
+
+let h1 = document.createElement('h1');
+h1.className = "heading"
+h1.textContent = "Chat";
+div.appendChild(h1);
+h1.style.color='white';
+h1.style.backgroundColor='pink';
+div.style.textAlign='left';
+h1.style.fontFamily='Roboto';
+h1.style.fontWeight='bold';
+h1.style.padding = '14px';
+h1.style.textAlign='center';
+
+
+let p1 = document.createElement('p');
+div.appendChild(p1);
+p1.textContent = "  Hello , how are you?";
+p1.style.backgroundColor='skyblue';
+p1.style.padding='20px';
+p1.style.borderTopLeftRadius = '12px';
+p1.style.borderTopRightRadius = '12px';
+p1.style.textAlign='left';
+
+let p2 = document.createElement('p');
+div.appendChild(p2);
+p2.textContent = "Hi Varakumar,I am good. How are you ?";
+p2.style.backgroundColor='white';
+p2.style.padding='20px';
+p2.style.borderTopLeftRadius = '12px';
+p2.style.borderTopRightRadius = '12px';
+p2.style.textAlign='right';
+
+let p3 = document.createElement('p');
+div.appendChild(p3);
+p3.textContent = " I'm fine,I am learning HTML and CSS";
+p3.style.backgroundColor='skyblue';
+p3.style.padding='20px';
+p3.style.borderTopLeftRadius = '12px';
+p3.style.borderTopRightRadius = '12px';
+p3.style.textAlign='left';
+
+let p4 = document.createElement('p');
+div.appendChild(p4);
+p4.textContent = "HTML and CSS";
+p4.style.backgroundColor='white';
+p4.style.padding='20px';
+p4.style.borderTopLeftRadius = '12px';
+p4.style.borderTopRightRadius = '12px';
+p4.style.textAlign='right';
+
+
+let p5 = document.createElement('p');
+div.appendChild(p5);
+p5.textContent = " Yes , by doing projects parallelly , I am learning DOM";
+p5.style.backgroundColor='skyblue';
+p5.style.padding='20px';
+p5.style.borderTopLeftRadius = '12px';
+p5.style.borderTopRightRadius = '12px';
+p5.style.textAlign='left';
+
+
+let p6 = document.createElement('p');
+div.appendChild(p6);
+p6.textContent = " Awesome,can you tell how you are learning it ";
+p6.style.backgroundColor='white';
+p6.style.padding='20px';
+p6.style.borderTopLeftRadius = '12px';
+p6.style.borderTopRightRadius = '12px';
+p6.style.textAlign='right';
+
+
+
+let p7 = document.createElement('p');
+div.appendChild(p7);
+p7.textContent = "Can I call you ?";
+p7.style.backgroundColor='skyblue';
+p7.style.padding='20px';
+p7.style.borderTopLeftRadius = '12px';
+p7.style.borderTopRightRadius = '12px';
+p7.style.textAlign='left';
+
+
+
+
+
+
+
+
